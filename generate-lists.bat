5c870db9ba75f9efcac7f5ff42622f02e2206561
loloC2C <44382811+loloC2C@users.noreply.github.com>
2018-12-01 00:30:18 +0100

    try change for Saint Seiya

--- generate-lists.bat
+++ generate-lists.bat
@@ -1,6 +1,6 @@
 @echo off
-xsltproc -o anime-list.xml transforms/create-anime-list.xsl anime-list-master.xml
-xsltproc -o anime-list-full.xml transforms/create-anime-list-full.xsl anime-list-master.xml
-xsltproc -o anime-list-unknown.xml transforms/create-anime-list-unknown.xsl anime-list-master.xml
-xsltproc -o anime-list-todo.xml transforms/create-anime-list-todo.xsl anime-list-master.xml
-xsltproc -o anime-movieset-list.xml transforms/update-anime-movieset-list.xsl anime-movieset-list.xml
+_tools\libxslt-1.1.26.win32\bin\xsltproc -o anime-list.xml transforms/create-anime-list.xsl anime-list-master.xml
+_tools\libxslt-1.1.26.win32\bin\xsltproc -o anime-list-full.xml transforms/create-anime-list-full.xsl anime-list-master.xml
+_tools\libxslt-1.1.26.win32\bin\xsltproc -o anime-list-unknown.xml transforms/create-anime-list-unknown.xsl anime-list-master.xml
+_tools\libxslt-1.1.26.win32\bin\xsltproc -o anime-list-todo.xml transforms/create-anime-list-todo.xsl anime-list-master.xml
+_tools\libxslt-1.1.26.win32\bin\xsltproc -o anime-movieset-list.xml transforms/update-anime-movieset-list.xsl anime-movieset-list.xml
